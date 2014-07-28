d354aaaed10376917b83f4ff4c0d725d57e1e1bf
Tomas <tomas@uow.edu.au>
2014-07-28 11:52:58 +1000

    Moved schema sceripts to cognicity-schema repo.

--- sql/createdb.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE DATABASE cognicity
-  WITH OWNER = postgres
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       LC_COLLATE = 'en_AU.UTF-8'
-       LC_CTYPE = 'en_AU.UTF-8'
-       CONNECTION LIMIT = -1;
