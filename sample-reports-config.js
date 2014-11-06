8c451e76902253035862716b1850a66d10cce4d1
Ben Jones <ben.jones@selera.com>
2014-11-06 14:14:01 +1100

    Documentation
    - Adding description of default log levels to config

--- sample-reports-config.js
+++ sample-reports-config.js
@@ -12,8 +12,7 @@ config.instance = 'cognicity-reports-powertrack';
 
 //Logging configuration
 config.logger = {};
-config.logger.level = "info"; // What level to log at; info, verbose or debug are most useful
-// TODO Refer to available levels
+config.logger.level = "info"; // What level to log at; info, verbose or debug are most useful. Levels are (npm defaults): silly, debug, verbose, info, warn, error.
 config.logger.maxFileSize = 1024 * 1024 * 100; // Max file size in bytes of each log file; default 100MB
 config.logger.maxFiles = 10; // Max number of log files kept
 
