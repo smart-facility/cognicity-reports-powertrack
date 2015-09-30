e27e779a0f67918f515ec837ba3c7ed9d528b694
Tomas <tomas@uow.edu.au>
2015-09-30 10:49:33 +1000

    moved test-config.js to test folder

--- test/test-config.js
+++ test/test-config.js
@@ -2,7 +2,7 @@
 
 // test-config.js - include the sample config and change some properties for test
 
-var config = require("./sample-reports-config.js");
+var config = require("../sample-reports-config.js");
 
 // Change instance name so we log to test.log
 config.logger.filename = 'test';
